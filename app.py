3ed0e286aa1c4825a0433f681602c1716def5e87
flame-mi <ycl3384897@gmail.com>
2026-01-08 21:53:36 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # 主页：渲染游戏页面
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # 启动开发服务器，debug 方便新手调试
+    app.run(debug=True)
